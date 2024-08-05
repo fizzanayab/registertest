9c76735646617f1ff504fba4d004273793a3c4da
fizza nayab <tab2baku@gmail.com>
2024-08-05 16:11:23 +0400

    ho

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,31 @@
 const toggleForm = () => {
     const container = document.querySelector('.container');
     container.classList.toggle('active');
-  };                                                             
+}
+let isChatboxUp = false;
+let togglebut = false;
+
+
+function toggleChatbox() {
+  const chatbox = document.getElementById("Chatbox");
+  if (isChatboxUp) {
+    chatbox.style.right = "0px"; 
+  } else {
+    chatbox.style.right = "-200px"; 
+  }
+  const toggleButton = document.getElementById("toggleButton");
+
+  if (togglebut) {
+    chatbox.style.right = "0px"; 
+  } else {
+    chatbox.style.right = "-200px"; 
+  }
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
